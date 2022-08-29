90d83b93318da4c2b7f6a29da01a870f479b7b1a
Franklin Obidike <Franklin@Obi-Franklin.local>
2022-08-29 23:48:50 +0100

    Who we are section

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,11 +1,60 @@
 import styled from 'styled-components'
 
-const FooterSection = styled.section``
+const FooterSection = styled.section`
+    padding: 4em 8em;
+    color: #FFFFFF;
+    background:#0E1428;
+    .logo{
+        margin:1rem 0;
+    }
+    h3{
+        font-size:24px;
+        margin-bottom:1rem;
+    }
+    input{
+        padding:1.5em 1em;
+        border:none;
+        outline:none;
+        width:300px;
+        border-top-left-radius:10px;
+        border-bottom-left-radius:10px;
+        ::placeholder{
+            color:grey;
+            font-size:16px;
+        }
+    }
+    button{
+        padding:1.5em 1em;
+        border:none;
+         border-top-right-radius:10px;
+        border-bottom-right-radius:10px;
+        background:#3EC5F0;
+        color:white;
+        width:150px;
+    }
+    .footerItems{
+        display:grid;
+        grid-template-columns:2fr 1fr 1fr 1fr;
+        h5{
+            font-size:20px;
+            margin-bottom:1rem;
+        }
+        p{
+            margin-bottom:.4rem;
+        }
+        .socialIcons{
+            margin-top:2rem;
+            img{
+                margin-right:12px
+            }
+        }
+    }
+`
 const Footer = () => {
     return (
         <FooterSection>
             <div className="logo">
-                <img src="./images/logo.svg" alt="" />
+                <img src="./images/footerlogo.svg" alt="" />
             </div>
             <div className="footerItems">
                 <div className="items">
